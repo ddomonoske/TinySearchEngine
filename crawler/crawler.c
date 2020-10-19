cd876f45c44d29ab7f88717622599e4bd86a368a
Eric Chen <eric.chen.21@dartmouth.edu>
2020-10-19 17:45:26 -0400

    step 5 nearly done

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -15,6 +15,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 
 /* fn() checks if the car_t pointer matches with the provided key
@@ -33,12 +34,30 @@ bool fn(void* p, const void* keyp) { //requires (void* elementp, const void* key
 }
 
 
+int32_t pagesave(webpage_t *pagep, int id, char *dirname) {
+	
+	FILE *fp;
+	char filename[10];
+
+	sprintf(filename, "%d", id); //convert int to string
+	
+	fp = fopen (filename,"w");
+	fprintf(fp,"URL: %s\nDepth: %d\nHTML Length: %d\nHTML: %s\n", webpage_getURL(pagep), webpage_getDepth(pagep), webpage_getHTMLlen(pagep), webpage_getHTML(pagep)); //prints URL, depth, HTML length, and HTML into file
+	
+	fclose(fp);
+	return 0;
+}
+
+
 int main (void) {
 
 	webpage_t *page; 	//initial page we fetch
 	webpage_t *intPage; //internal page
 	webpage_t *tmpPage; //used to delete the webpages in the queue
 	webpage_t *searchResult = NULL;
+	
+	page = (webpage_new("https://thayer.github.io/engs50/", 0, NULL)); //create new webpage_t of depth 0
+
 	
 	queue_t *qp; //queue to store webpage_t addresses
 	int pos = 0;
@@ -47,37 +66,33 @@ int main (void) {
 	hashtable_t *hp; //hash table to store URLs already fetched
 	uint32_t hsize = 6;
 	
-
-	page = (webpage_new("https://thayer.github.io/engs50/", 0, NULL)); //create new webpage_t of depth 0
-	
-	
-	/* 
-	****************
-	Insert step 5 code to save page
-	****************
-	*/
-	
-	
 	qp = qopen();
 	hp = hopen(hsize);
+	
 	
 	printf("Internal and External URLs: \n");
 
 	if(webpage_fetch(page)) { //fetch webpage HTML of new webpage_t, returns true if success
+	
+		//STEP 5
+    	uint32_t save;
+		save = pagesave(page, 1, "filler");
+		//printf("\n\nSave status: %d\n\n", save);
 	
     	while((pos = webpage_getNextURL(page, pos, &URLresult)) > 0) { //go through webpage looking for embedded URLs
     	//assign each URL string found to URLresult, continue until no more embedded URLs
     	
     		//STEP 4
     		searchResult = hsearch(hp, fn, URLresult, strlen(URLresult)); //search for URL in hashtable, return NULL if not found
+    		
     		if (IsInternalURL(URLresult) && searchResult == NULL) { //check to see if URL is internal
     			printf("Internal url: %s\n", URLresult);
     			hput(hp, URLresult, URLresult, strlen(URLresult)); //add new URL into hashtable (use URL itself as hash key bc key must be a char)
 				
 				//STEP 3
 				intPage = (webpage_new(URLresult, 0, NULL)); //create new webpage_t for each internal URL, assign correct depth
+    			webpage_fetch(intPage); //store HTML into webpage_t as well
     			qput(qp, intPage); //place new webpage_t into queue
-    			
 	    	} 
     		else {	//if URL is external
     			printf("External url: %s\n", URLresult);
@@ -87,11 +102,12 @@ int main (void) {
 
 
     	
-    	printf("\n\n\nStep 3 Verification: \n");
+    	printf("\n\nStep 3 Verification: \n");
     	
     	//use qget to remove each webpage_t address from the queue and print each associated URL
     	while((tmpPage = (webpage_t*)qget(qp)) != NULL){ //while queue is not empty
     		printf("%s\n", webpage_getURL(tmpPage));  //print URL
+    		printf("HTML Length: %d\n", webpage_getHTMLlen(tmpPage)); //print HTML length
     		webpage_delete((void*)tmpPage); //delete webpage_t referenced by tmpPage address
     	}
     	
