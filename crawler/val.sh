d83f12705564d34f074a114bed46a69ca27cf0e6
David Domonoske <david.w.domonoske.th@dartmouth.edu>
2020-10-20 10:37:14 -0400

    added line to create directory before running test

--- crawler/val.sh
+++ crawler/val.sh
@@ -2,8 +2,9 @@
 echo making object files and executables...
 make &>> /dev/null
 
+mkdir test_dir
 echo running valgrind...
-valgrind --leak-check=full --show-leak-kinds=all crawler https://thayer.github.io/engs50/ test_dir 1 &> valout.txt
+valgrind --leak-check=full --show-leak-kinds=all crawler https://thayer.github.io/engs50/ test_dir 0 &> valout.txt
 grep "ERROR SUMMARY\| Command: \| no leaks are possible" valout.txt
 
 rm valout.txt
