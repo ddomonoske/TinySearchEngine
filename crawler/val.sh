ad9cce0af6db22e645991849699468359f4c30f2
David Domonoske <david.w.domonoske.th@dartmouth.edu>
2020-10-20 09:39:24 -0400

    modified val.sh to be valid test case

--- crawler/val.sh
+++ crawler/val.sh
@@ -3,7 +3,9 @@ echo making object files and executables...
 make &>> /dev/null
 
 echo running valgrind...
-valgrind --leak-check=full --show-leak-kinds=all crawler test tester_dir 5
+valgrind --leak-check=full --show-leak-kinds=all crawler https://thayer.github.io/engs50/ test_dir 1 &> valout.txt
+grep "ERROR SUMMARY\| Command: \| no leaks are possible" valout.txt
 
+rm valout.txt
 echo making clean...
 make clean &>> /dev/null
