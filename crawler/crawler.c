d1cca8e69ab5c2378ac529d9f6edceece6b90334
David Domonoske <david.w.domonoske.th@dartmouth.edu>
2020-10-20 03:07:02 -0400

    crawler.c now crawls to webpages, still need to save pages

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -18,6 +18,7 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
+typedef struct stat stat_t;
 
 /* fn() checks if the car_t pointer matches with the provided key
  * returns bool
@@ -45,7 +46,7 @@ int32_t pagesave(webpage_t *pagep, int id, char *dirname) {
 
 	sprintf(filename, "%d", id); //convert int to string
 	
-	chdir(".."); 
+	//chdir("..");  // removed because I believe the ../ should be included in the directory name
 	chdir(dirname); //changes over to right directory
 	
 	fp = fopen (filename,"w");
@@ -56,7 +57,26 @@ int32_t pagesave(webpage_t *pagep, int id, char *dirname) {
 }
 
 
-int main (void) {
+/* to check if the directory under string beginning at *dir exists
+ * returns true if it does, false if not
+ * used for STEP 6
+ */
+bool isDirExist(char *dir) {
+	stat_t s;
+	bool b;
+	if(!stat(dir,&s)) {
+		if (S_ISDIR(s.st_mode))
+			b =  true;
+		else
+			b = false;
+	} else
+		b = false;
+
+	return b;
+}
+
+
+int main (int argc, char *argv[]) {
 
 	webpage_t *page; 	//initial page we fetch
 	webpage_t *intPage; //internal page
@@ -66,6 +86,10 @@ int main (void) {
 	queue_t *qp; //queue to store webpage_t addresses
 	hashtable_t *hp; //hash table to store URLs already fetched
 	uint32_t hsize = 6;
+
+	char *seedurl;
+	char *pagedir;
+	uint32_t maxdepth, currdepth=0;
 	
 	page = (webpage_new("https://thayer.github.io/engs50/", 0, NULL)); //create new webpage_t of depth 0
 	qp = qopen();
@@ -73,58 +97,83 @@ int main (void) {
 	
 	int pos = 0; //used for webpage_getNextURL function
 	char *URLresult; //pointer to string, used to store each embedded URL during getNextURL
-	
+
+	// Checking inputs for STEP 6
+	if (argc != 4) {
+		printf("ERROR: Incorrect number of arguments.\n");
+		printf("USAGE: crawler <seedurl> <pagedir> <maxdepth>\n");
+		exit(EXIT_FAILURE);
+	}
+	seedurl = argv[1];
+	pagedir = argv[2];
+	if ( ! isDirExist(argv[2]) ) {
+		printf("The directory '%s' does not exist.\n", argv[2]);
+		exit(EXIT_FAILURE);
+	}
+	if (atoi(argv[3]) < 0) {
+		printf("The max depth must be greater than 0.\n");
+		exit(EXIT_FAILURE);
+	} else
+		maxdepth = atoi(argv[3]);
 	
 	printf("Internal and External URLs: \n");
 
 	if(webpage_fetch(page)) { //fetch webpage HTML of new webpage_t, returns true if success
-	
+
 		//STEP 5
-    	uint32_t save;
-		save = pagesave(page, 1, "pages");
+		uint32_t save;
+		save = pagesave(page, 1, "../pages");
 		printf("\nSave status: %d\n\n", save);
-	
-    	while((pos = webpage_getNextURL(page, pos, &URLresult)) > 0) { //go through webpage looking for embedded URLs
-    	//assign each URL string found to URLresult, continue until no more embedded URLs
-    	
-    		//STEP 4
-    		searchResult = hsearch(hp, fn, URLresult, strlen(URLresult)); //search for URL in hashtable, return NULL if not found
-    		
-    		if (IsInternalURL(URLresult) && searchResult == NULL) { //check to see if URL is internal
-    			printf("Internal url: %s\n", URLresult);
-    			hput(hp, URLresult, URLresult, strlen(URLresult)); //add new URL into hashtable (use URL itself as hash key bc key must be a char)
+
+		do {
+			while ((pos = webpage_getNextURL(page, pos, &URLresult)) > 0) { //go through webpage looking for embedded URLs
+				//assign each URL string found to URLresult, continue until no more embedded URLs
+
+				//STEP 4
+				searchResult = hsearch(hp, fn, URLresult, strlen(URLresult)); //search for URL in hashtable, return NULL if not found
 				
-				//STEP 3
-				intPage = (webpage_new(URLresult, 0, NULL)); //create new webpage_t for each internal URL, assign correct depth
-    			webpage_fetch(intPage); //store HTML into webpage_t as well
-    			qput(qp, intPage); //place new webpage_t into queue
-	    	} 
-    		else {	//if URL is external
-    			printf("External url: %s\n", URLresult);
-    			free(URLresult);
-    		}
-    	}
-
-
+				if (IsInternalURL(URLresult) && searchResult == NULL) { //check to see if URL is internal
+					printf("Internal url: %s\n", URLresult);
+					hput(hp, URLresult, URLresult, strlen(URLresult)); //add new URL into hashtable (use URL itself as hash key bc key must be a char)
+					
+					//STEP 3
+					intPage = (webpage_new(URLresult, currdepth+1, NULL)); //create new webpage_t for each internal URL, assign correct depth
+					webpage_fetch(intPage); //store HTML into webpage_t as well
+					qput(qp, intPage); //place new webpage_t into queue
+				} 
+				else {	//if URL is external
+					//printf("External url: %s\n", URLresult);
+					free(URLresult);
+				}
+			}
+			if ((page = qget(qp)) == NULL)
+				currdepth = maxdepth + 1;
+			else
+				currdepth = webpage_getDepth(page);
+			printf("next URL: %s\n", webpage_getURL(page));
+			printf("currdepth = %d\n", currdepth);
+			pos = 0;
+		} while (currdepth <= maxdepth); 
+		
+    
+		printf("\n\nStep 3 Verification: \n");
     	
-    	printf("\n\nStep 3 Verification: \n");
-    	
-    	//use qget to remove each webpage_t address from the queue and print each associated URL
-    	while((tmpPage = (webpage_t*)qget(qp)) != NULL){ //while queue is not empty
-    		printf("%s\n", webpage_getURL(tmpPage));  //print URL
-    		printf("HTML Length: %d\n", webpage_getHTMLlen(tmpPage)); //print HTML length
-    		webpage_delete((void*)tmpPage); //delete webpage_t referenced by tmpPage address
-    	}
-    	
-    	//DEALLOCATE all memory used
-    	qclose(qp); //close queue
-    	hclose(hp); //close hashtable
-    	webpage_delete((void*)page); //delete original webpage_t
-    	
-    	exit(EXIT_SUCCESS); 
-  	}
-  	
-  	else exit(EXIT_FAILURE); //EXIT_FAILURE if fetch of seed URL does not succeed
+		//use qget to remove each webpage_t address from the queue and print each associated URL
+		while((tmpPage = (webpage_t*)qget(qp)) != NULL){ //while queue is not empty
+			printf("%s\n", webpage_getURL(tmpPage));  //print URL
+			printf("HTML Length: %d\n", webpage_getHTMLlen(tmpPage)); //print HTML length
+			webpage_delete((void*)tmpPage); //delete webpage_t referenced by tmpPage address
+		}
+    
+		//DEALLOCATE all memory used
+		qclose(qp); //close queue
+		hclose(hp); //close hashtable
+		webpage_delete((void*)page); //delete original webpage_t
+    
+		exit(EXIT_SUCCESS); 
+	}
+  
+	else exit(EXIT_FAILURE); //EXIT_FAILURE if fetch of seed URL does not succeed
 	
 }
 
