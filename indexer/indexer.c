f6a84bfb3ce96bf2a442bc505e5649beb8e2f51b
Chris Lyke <christopher.m.lyke.21@dartmouth.edu>
2020-10-25 19:32:55 -0400

    step 4

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -24,9 +24,16 @@
 
 int sumGlobal = 0;
 
+/* the representation of a doc for the queue (step 4) */
+typedef struct counters {
+	int id;
+	uint32_t count;
+} counters_t;
+
+
 typedef struct wc {
 	char *word;
-	uint32_t count;
+	queue_t *qp;
 } wc_t;
 
 
@@ -66,7 +73,7 @@ bool wordmatch(void* elementp, const void* word) {
 /* make a wc_t object
  *   - malloc space for object and for word
  */
-wc_t* make_wc(char *word, uint32_t count) {
+wc_t* make_wc(char *word, counters_t *qp) {
 	wc_t *wcp;
 	uint32_t wordLength;
 	if ((wcp = (wc_t*)malloc(sizeof(wc_t))) == NULL) { 	//check if enough space in memory for car_t*
@@ -80,7 +87,7 @@ wc_t* make_wc(char *word, uint32_t count) {
 		return NULL;
 	}
 	strcpy(wcp->word, word);
-	wcp->count = count;
+	wcp->qp = qp;
 	return wcp;
 }
 
@@ -91,15 +98,43 @@ wc_t* make_wc(char *word, uint32_t count) {
 void freeWord(void* elementp) {
 	wc_t *wcp = (wc_t*)elementp;
 	free(wcp->word);
+	qclose(wcp->qp); //step 4
+}
+
+/* add the count of words in each queue element to the sum (should be 141)
+ *   -used with qapply for step4
+ */
+void sumwordsQueue(void* elementp) {
+	counters_t *docp = (counters_t*)elementp;
+	sumGlobal += docp->count;
+}
+
+/* call sumwordsQueue for each queue in the hashtable
+ */
+void sumwordsHash(void* elementp) {
+	wc_t *wcp = (wc_t*)elementp;
+	qapply(wcp->qp, sumwordsQueue);
 }
 
 
-/* add the count of words in each element to the sum (should be 141)
- *   -used with happly
+/* make_doc(): creates a doc object for queue(step 4) */
+counters_t* make_doc(int id, uint32_t count) {
+	counters_t *doc;
+	if ((doc = (counters_t*)malloc(sizeof(counters_t))) == NULL) { //check if enough space in memory for counters_t*
+		printf("Error: malloc failed allocating car\n");
+		return NULL;
+	}
+	doc->id = id;
+	doc->count = count;
+	return doc;
+}
+
+/* look in queue for element with matching doc ID
  */
-void sumWords(void* elementp) {
-	wc_t *wcp = (wc_t*)elementp;
-	sumGlobal += wcp->count;
+bool QueueSearchFn(void* elementp, const void* docID) { //requires (void* elementp, const void* keyp)
+	counters_t *doc = (counters_t*)elementp;
+	int *id = (int*)docID;
+	return (doc->id == *id); //if match return true
 }
 
 
@@ -107,34 +142,51 @@ int main(void) {
 	webpage_t *page;
 	char *word;
 	int pos = 0;
+	int id = 1;
 	
 	//FILE *fp;
 	hashtable_t *hp = hopen(HTABLE_SIZE);  // Step 3
 	wc_t *wc_found;
+	queue_t *qp;
+	counters_t *doc_found;
 
-	page = pageload(1,"../pages");
+	page = pageload(id,"../pages");
 
 	//chdir("../indexer");  // for Step 2
 	//fp = fopen("output1","w"); // for Step 2
 		
 	while ((pos = webpage_getNextWord(page, pos, &word)) > 0) { //go through every word in html
-		if (NormalizeWord(word) == true) {  // only add to hash table if normalized
+		if (NormalizeWord(word)) {  // only add to hash table if normalized
 			if ((wc_found = hsearch(hp, wordmatch, word, strlen(word))) == NULL) {
 				printf("adding new word to hash table\n");
-				hput(hp, make_wc(word,1), word, strlen(word));
+				qp = qopen(); //make new queue
+				qput(qp, make_doc(id,1)); //place doc element in queue
+				hput(hp, make_wc(word, qp), word, strlen(word)); //put queue in word element in hashtable
+				
 			} else {
 				printf("increasing the count for '%s' by 1\n", word);
-				(wc_found->count)++;
+				
+				//look for element in queue with matching doc ID
+				doc_found = qsearch(wc_found->qp, QueueSearchFn, &id); 
+				if((doc_found) != NULL){
+					(doc_found->count)++; //add to count inside doc element in queue		
+				}	
+				else { //if no element in the queue, make a new doc element
+					qp = qopen();
+					qput(qp, make_doc(id,1));  //place doc in queue
+					hput(hp, make_wc(word, qp), word, strlen(word)); //place queue in hashtable
+				}
 			}
 			//fprintf(fp, "%s\n", word);  // remnant from Step 2
 		}
 		free(word); 
 	}
 	
-	//Calculate sum of all counts in the hash table
-	happly(hp, sumWords);
-	printf("*** %d words ***\n", sumGlobal);
 	
+	//Step 4: Calculate sum of all counts in the hash table and internal queues
+	happly(hp, sumwordsHash);
+	printf("*** %d words ***\n", sumGlobal);
+
 	
 	webpage_delete(page);
 	
