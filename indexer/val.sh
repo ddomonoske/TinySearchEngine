847fc425725394504ded7dde5afcde3ab2bedaf8
David Domonoske <david.w.domonoske.th@dartmouth.edu>
2020-10-28 15:33:56 -0400

    Added a test for indexer_test

--- indexer/val.sh
+++ indexer/val.sh
@@ -2,9 +2,12 @@
 echo making object files and executables...
 make &>> /dev/null
 
-mkdir test_dir
 echo running valgrind...
-valgrind --leak-check=full --show-leak-kinds=all indexer 2
+valgrind --leak-check=full --show-leak-kinds=all indexer 2 &> memcheck.txt
+grep "ERROR SUMMARY\| Command: \| no leaks are possible" memcheck.txt; echo
+valgrind --leak-check=full --show-leak-kinds=all indexer_test &> memcheck.txt
+grep "ERROR SUMMARY\| Command: \| no leaks are possible" memcheck.txt; echo
 
+rm memcheck.txt
 echo making clean...
 make clean &>> /dev/null
