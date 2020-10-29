40abee4f53d00018fad75444e957a4117915ae2a
David Domonoske <david.w.domonoske.th@dartmouth.edu>
2020-10-29 03:02:51 -0400

    Now has tests for indexer and indexer_test

--- indexer/val.sh
+++ indexer/val.sh
@@ -2,10 +2,12 @@
 echo making object files and executables...
 make &>> /dev/null
 
-echo running valgrind...
-valgrind --leak-check=full --show-leak-kinds=all indexer 2 &> memcheck.txt
+echo running valgrind...; echo
+valgrind --leak-check=full --show-leak-kinds=all indexer ../pages indexName1 &> memcheck.txt
 grep "ERROR SUMMARY\| Command: \| no leaks are possible" memcheck.txt; echo
-valgrind --leak-check=full --show-leak-kinds=all indexer_test &> memcheck.txt
+valgrind --leak-check=full --show-leak-kinds=all indexer_test indexName1 indexName2 &> memcheck.txt
+grep "ERROR SUMMARY\| Command: \| no leaks are possible" memcheck.txt; echo
+valgrind --leak-check=full --show-leak-kinds=all indexer_test badName indexName4 &> memcheck.txt
 grep "ERROR SUMMARY\| Command: \| no leaks are possible" memcheck.txt; echo
 
 rm memcheck.txt
