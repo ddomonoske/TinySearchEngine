21c715c656900fbb44b291fe45ea14f432128520
Eric Chen <eric.chen.21@dartmouth.edu>
2020-10-29 12:59:34 -0400

    fixed pageload() function to read from saved files already fetched by crawler, instead of re-fetching the webpage

--- utils/pageio.c
+++ utils/pageio.c
@@ -50,7 +50,7 @@ int32_t pagesave(webpage_t *pagep, int id, char *dirname) {
 	chdir(dirname); //changes over to right directory
 	
 	fp = fopen (filename,"w");
-	fprintf(fp,"%s\n%d\n%d\n%s\n", webpage_getURL(pagep), webpage_getDepth(pagep),
+	fprintf(fp,"%s\n%d\n%d\n%s", webpage_getURL(pagep), webpage_getDepth(pagep),
 					webpage_getHTMLlen(pagep), webpage_getHTML(pagep)); //prints URL, depth, HTML length, and HTML into file
 	printf("pagesave: %s\n", webpage_getURL(pagep));
 
@@ -68,30 +68,54 @@ int32_t pagesave(webpage_t *pagep, int id, char *dirname) {
 webpage_t *pageload(int id, char *dirnm) {
 
 	FILE *fp;
-	webpage_t *page;
 	char filename[10];
-	char urlstr[100];
-	char depthstr[10];
-	int depth;
-	char cwd[MAX_PATH_LENGTH];
-
-	getcwd(cwd, sizeof(cwd));
-	chdir(dirnm); //changes over to right directory
+	char URL[200]; 	//200 is long enough
+	int depth, htmlLength;
+	char cwd[MAX_PATH_LENGTH]; 
+	
+	getcwd(cwd, sizeof(cwd)); //tracks the current working directory (would be crawler in this case)	
+	chdir(dirnm); //changes over to directory from which we are loading filenames from (../pages in this case)
+	
 	sprintf(filename, "%d", id); //convert int to string
+	fp = fopen(filename,"r"); //open id file saved in pages
 	
-	fp = fopen(filename,"r"); 
-	fscanf(fp, "%s %s", urlstr, depthstr); //stores URL and depth info
-	//printf("URL is %s \n Depth is %s\n", urlstr, depthstr);
-	depth = atoi(depthstr);
-	fclose(fp);
-	
-	page = (webpage_new(urlstr, depth, NULL)); //create new webpage_t
-	printf("pageload: %s\n", webpage_getURL(page));
-
-	if (webpage_fetch(page)) {
-		return page;
+	if (fp == NULL) {
+		printf("File is NULL");
+		return NULL;
 	}
-	else return NULL; //if webpage_fetch fails
+	
+	fscanf(fp, "%s %d %d", URL, &depth, &htmlLength); //captures URL, depth, and HTML Length info frome file
+	//printf("URL is %s \nDepth is %d\nHtml Length is %d\n", URL, depth, htmlLength);
 
-	chdir(cwd);  // return to the original cwd
+	//CAPTURING HTML FROM FILE
+	char *html = (char*)malloc(sizeof(char)*htmlLength+1); 	//need to allocate memory for HTML of new wegpage_t
+	char c;
+	int i = 0;
+
+   	while ( (c = fgetc(fp)) != EOF) { //scans each remaining character in the file until hits EOF
+   		html[i] = c; 
+   		i++;
+   	}
+   	html[i-1] = '\0'; //rewrite end of html string (change from EOF to '\0')
+   	
+   	fclose(fp);
+	chdir(cwd);  // changes back to the original cwd
+   	  
+   	return (webpage_new(URL, depth, html)); //returns new webpage_t, or NULL if any error
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
