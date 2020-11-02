1000c4de90e220df8d83ca9c828f7469779d2a38
David Domonoske <david.w.domonoske.th@dartmouth.edu>
2020-11-02 11:45:05 -0500

    Finished Step 2

--- querier/querier.c
+++ querier/querier.c
@@ -50,7 +50,11 @@ int main (void) {
 	char *words[10]; //array to store input (max input = 10 words)
 	
 	char delimits[] = " \t\n"; //set delimiters as space and tab
-	int i,j; //to store and print words from words array
+	char unique[51]; // unique word from index file
+	int i,j,wc; //to store and print words from words array
+	int id,count,min,status;
+	
+	FILE *fp;
 	
 	printf(" > ");	
 
@@ -59,24 +63,57 @@ int main (void) {
 	
 		if (NormalizeWord(input)) { //input can only contain alphabetic-characters and white space
     		
-			i=0; //rewrite words array for each new input 
-			words[i] = strtok(input,delimits); //strtok() splits input according to delimits and returns next token
+			wc=0; //rewrite words array for each new input 
+			words[wc] = strtok(input,delimits); //strtok() splits input according to delimits and returns next token
 			
-			while (words[i] != NULL) { //stores all input words into words array
+			while (words[wc] != NULL) { //stores all input words into words array
 				//printf("%s ",words[i]); 
-				i++;
-				words[i] = strtok(NULL,delimits); //continue splitting input until strktok returns NULL (no more tokens)
+				wc++;
+				words[wc] = strtok(NULL,delimits); //continue splitting input until strktok returns NULL (no more tokens)
 			}
 			
-			for(j = 0; j<i; j++) { //print words in words array
+			for(j = 0; j<wc; j++) { //print words in words array
 				printf("%s ", words[j]);
 			}
-		}
+			printf("\n");
+
+			//STEP 2
+			fp = fopen("indexForQuery","r");
+			for ( i=0; i<wc; i++) { // looping through all query words
+				rewind(fp); // restart at beginning of file
+				j=0;
+				
+				do { // scan through each line of file
+					fscanf(fp, "%50s", unique);  // get word, no longer than 50 characters
+						
+					// loop until end of line. End of line determined by failing to match two integers
+					while ((status = fscanf(fp, "%d %d", &id, &count)) == 2) {
+						
+						if (strcmp(words[i],unique)==0) { // if unique is a query match
+							printf("%s:%d ", unique, count);
+								
+						}
+					} // end scan of line
+
+					if (j==0) { // update min to smallest amount
+						min = count;
+						j = 1;
+					}
+					if (count < min)
+						min = count;		
+
+				} while (status != EOF); // end scan of file
+
+			}
+			
+			printf("-- %d\n",min);
+
+		} // end of valid query search
 		else printf("[invalid query]"); //reject queries containing non-alphabetic/non-whitespace characters
-		
+			
 		printf("\n > ");
 	}
-	
+		
 	printf("\n"); //add new line after user terminates with ctrl+d
 	return(0);
 	
