17da88d53578369c329f5732aa9afb77a966deba
David Domonoske <david.w.domonoske.th@dartmouth.edu>
2020-11-04 12:49:58 -0500

    Added val.sh to querier/

---
+++ querier/val.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo making object files and executables...
+make &>> /dev/null
+
+echo running valgrind...; echo
+valgrind --leak-check=full --show-leak-kinds=all querier #&> memcheck.txt
+#grep "ERROR SUMMARY\| Command: \| no leaks are possible" memcheck.txt; echo
+#valgrind --leak-check=full --show-leak-kinds=all indexer_test indexName1 indexName2 #&> memcheck.txt
+#grep "ERROR SUMMARY\| Command: \| no leaks are possible" memcheck.txt; echo
+#valgrind --leak-check=full --show-leak-kinds=all indexer_test badName indexName4 #&> memcheck.txt
+#grep "ERROR SUMMARY\| Command: \| no leaks are possible" memcheck.txt; echo
+
+rm memcheck.txt
+echo making clean...
+make clean &>> /dev/null
