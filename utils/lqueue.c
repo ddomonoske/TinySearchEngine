00720118c630739813474c75be1bd5a49b1b2c10
Eric Chen <eric.chen.21@dartmouth.edu>
2020-11-10 12:01:17 -0500

    Step 1 started - created lqueue.h, implemented half the functions in lqueue.c, need to complete then test

---
+++ utils/lqueue.c
@@ -0,0 +1,136 @@
+/* 
+ * queue.c -- implementation to the locked queue module
+ */
+
+#include "queue.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "lqueue.h"
+
+static bool print_message = false; // control printing to screen
+
+/* 
+ * - prints message to screen with a prefix that includes file name
+ * - only prints to screen when print_message is true
+ */
+static void print(char *message) {
+	if (print_message) {
+		fprintf(stderr,"~hash.c -> ");
+		fprintf(stderr,"%s\n", message);
+	}
+}
+
+
+/* defines each node of a queue */
+typedef struct lqueue{
+	queue_t *qp;	//queue
+	pthread_mutex_t m;  //associated mutex lock
+} lqheader_t;
+
+
+
+/* create an empty locked queue */
+lqueue_t* lqopen(void) {
+
+	lqheader_t *lqp;
+	if ( (lqp = (lqheader_t*)malloc(sizeof(lqheader_t))) == NULL ) //allocate space for lqueue_t object
+		return NULL;	
+	lqp->qp = qopen(); 
+	pthread_mutex_init(&(lqp->m),NULL); //initialize mutex
+	print("New locked queue created\n");
+
+	return (lqueue_t*)lqp;
+	
+}      
+
+/* deallocate a locked queue, frees everything in it */
+void lqclose(lqueue_t *lqp) {
+
+	lqheader_t *hp;
+	
+	if (lqp == NULL) {
+		print("Error: locked queue is NULL\n");
+	}
+	else {
+		hp = (lqheader_t*)lqp;
+		qclose(hp->qp);
+		pthread_mutex_destroy(&(hp->m));/* destroy the mutex */
+		free(hp);
+		print("Locked queue closed\n");
+	}
+} 
+
+/* put element at the end of the lqueue
+ * returns 0 is successful; nonzero otherwise 
+ */
+int32_t lqput(lqueue_t *lqp, void *elementp) {
+
+	lqheader_t *hp;	
+	int32_t status;
+	
+	if (lqp == NULL) {	
+		print("Error: locked queue is NULL\n");
+		return 1;
+	}
+	
+	if (elementp == NULL) {	
+		print("Error: object passed in is NULL\n");
+		return 1;
+	}
+
+	hp = (lqheader_t*)lqp; //case lqp into local object type
+
+	pthread_mutex_lock(&(hp->m));
+	status = qput(hp->qp,elementp); //puts elementp into lqp
+	pthread_mutex_unlock(&(hp->m));
+	
+	return(status);
+}
+
+
+
+/* get the first first element from lqueue, removing it from the lqueue */
+void* lqget(lqueue_t *lqp) {
+
+	lqheader_t *hp;	
+	
+	if (lqp == NULL) {	
+		print("Error: queue is NULL\n");
+		return NULL;
+	}
+	
+	hp = (lqheader_t*)lqp;
+	return (qget(hp->qp)); //returns element from lqueue
+}
+
+
+/* apply a function to every element of the queue */
+void lqapply(lqueue_t *lqp, void (*fn)(void* elementp));
+
+/* search a queue using a supplied boolean function
+ * skeyp -- a key to search for
+ * searchfn -- a function applied to every element of the queue
+ *          -- elementp - a pointer to an element
+ *          -- keyp - the key being searched for (i.e. will be 
+ *             set to skey at each step of the search
+ *          -- returns TRUE or FALSE as defined in bool.h
+ * returns a pointer to an element, or NULL if not found
+ */
+void* lqsearch(lqueue_t *lqp, 
+							bool (*searchfn)(void* elementp,const void* keyp),
+							const void* skeyp);
+
+// NOT MENTIONED IN MODULE STEP
+/* search a queue using a supplied boolean function (as in qsearch),
+ * removes the element from the queue and returns a pointer to it or
+ * NULL if not found
+ */
+void* lqremove(lqueue_t *lqp,
+							bool (*searchfn)(void* elementp,const void* keyp),
+							const void* skeyp);
+
+/* concatenatenates elements of q2 into q1
+ * q2 is dealocated, closed, and unusable upon completion 
+ */
+void lqconcat(lqueue_t *q1p, lqueue_t *q2p);
